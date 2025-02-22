c3878c84ece4f969b7e6177baea5292b0bca9a25
dhame11 <43104499+dhame11@users.noreply.github.com>
2025-02-22 14:39:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,3 @@
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_vpc" "vpc-hcl" {
   cidr_block = "10.0.0.0/16"
 }
